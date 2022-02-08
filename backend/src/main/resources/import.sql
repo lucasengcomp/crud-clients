884a2349aadc250f02c60c1ae5da33b1c1c643e1
Lucas Galvao Lima <lucas.engcomp@outlook.com>
2022-02-08 00:05:32 -0300

    Client. Seeding database.

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,15 @@
+/*
+    INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Jhon Deacon', '00011122230', 350.01, '1951-08-19T00:00:00Z', 6);
+    INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Farrokh Bulsara', '00011122231', 350.01, '1946-09-05T00:00:00Z', 0);
+    INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Roger Taylor', '00011122232', 350.01, '1949-07-26T00:00:00Z', 5);
+    INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Brian May', '00011122233', 350.01, '1947-07-19T00:00:00Z', 3);
+    INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Ambrosina Orozina', '00011122234', 50.01, '1999-03-17T00:00:00Z', 8);
+    INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Paulo Souza', '00011122235', 250.01, '1995-05-11T00:00:00Z', 1);
+*/
+
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Jhon Deacon', '00011122230', 350.01, '1951-08-19', 6);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Farrokh Bulsara', '00011122231', 350.01, '1946-09-05', 0);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Roger Taylor', '00011122232', 350.01, '1949-07-26', 5);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Brian May', '00011122233', 350.01, '1947-07-19', 3);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Ambrosina Orozina', '00011122234', 50.01, '1999-03-17', 8);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Paulo Souza', '00011122235', 250.01, '1995-05-11', 1);
