8278e76a3bb8a028d44a6d10e8675e341b84aa3f
Lucas Galvao Lima <lucas.engcomp@outlook.com>
2022-02-13 12:03:14 -0300

    Handling for throwing errors when resource not found

---
+++ backend/src/main/java/com/br/clients/resource/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.br.clients.resource.exceptions;
+
+import com.br.clients.services.exceptions.ResourceNotFoundException;
+import com.br.clients.services.exceptions.StandardError;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.Instant;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+        StandardError error = new StandardError();
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        error.setTimestamp(Instant.now());
+        error.setStatus(status.value());
+        error.setError("Resource not found");
+        error.setMesssage(e.getMessage());
+        error.setPath(request.getRequestURI());
+        return ResponseEntity.status(status).body(error);
+    }
+}
--- backend/src/main/java/com/br/clients/services/ClientService.java
+++ backend/src/main/java/com/br/clients/services/ClientService.java
@@ -3,6 +3,7 @@ package com.br.clients.services;
 import com.br.clients.dto.ClientDTO;
 import com.br.clients.entities.Client;
 import com.br.clients.repositories.ClientRepostory;
+import com.br.clients.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,7 +17,7 @@ public class ClientService {
 
     public ClientDTO findById(Long id) {
         Optional<Client> client = repostory.findById(id);
-        Client entity = client.orElseThrow(() -> new RuntimeException("Entity not found!"));
+        Client entity = client.orElseThrow(() -> new ResourceNotFoundException("Entity not found!"));
         return new ClientDTO(entity);
     }
 }
